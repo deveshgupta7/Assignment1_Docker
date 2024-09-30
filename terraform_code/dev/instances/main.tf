b398cc9af35a36d9b33d5b20835573b31964ad3a
EC2 Default User <ec2-user@ip-172-31-83-86.ec2.internal>
2024-09-30 03:45:11 +0000

    Your commit message

---
+++ terraform_code/dev/instances/main.tf
@@ -0,0 +1,142 @@
+#  Define the provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Data source for AMI id
+data "aws_ami" "latest_amazon_linux" {
+  owners      = ["amazon"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+
+# Data source for availability zones in us-east-1
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+# Data block to retrieve the default VPC id
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Define tags locally
+locals {
+  default_tags = merge(module.globalvars.default_tags, { "env" = var.env })
+  prefix       = module.globalvars.prefix
+  name_prefix  = "${local.prefix}-${var.env}"
+}
+
+# Retrieve global variables from the Terraform module
+module "globalvars" {
+  source = "../../modules/globalvars"
+}
+
+# Reference subnet provisioned by 01-Networking 
+resource "aws_instance" "my_amazon" {
+  ami                         = data.aws_ami.latest_amazon_linux.id
+  instance_type               = lookup(var.instance_type, var.env)
+  key_name                    = aws_key_pair.my_key.key_name
+  vpc_security_group_ids      = [aws_security_group.my_sg.id]
+  associate_public_ip_address = true
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = merge(local.default_tags,
+    {
+      "Name" = "${local.name_prefix}-Amazon-Linux"
+    }
+  )
+}
+
+
+# Adding SSH key to Amazon EC2
+resource "aws_key_pair" "my_key" {
+  key_name   = local.name_prefix
+  public_key = file("${local.name_prefix}.pub")
+}
+
+# Security Group
+resource "aws_security_group" "my_sg" {
+  name        = "allow_ssh_http_https_sql"
+  description = "Allow SSH, HTTP, HTTPS, and SQL inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description      = "SSH from everywhere"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTP from everywhere"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTPS from everywhere"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "SQL (MySQL) from specific IP"
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = merge(local.default_tags,
+    {
+      "Name" = "${local.name_prefix}-sg"
+    }
+  )
+}
+
+
+# Elastic IP
+resource "aws_eip" "static_eip" {
+  instance = aws_instance.my_amazon.id
+  tags = merge(local.default_tags,
+    {
+      "Name" = "${local.name_prefix}-eip"
+    }
+  )
+}
+
+# Create AWS ECR Repository
+resource "aws_ecr_repository" "my_ecr_repo" {
+  name                 = "${local.name_prefix}-ecr" # Ensure this is a valid name
+  image_tag_mutability = "MUTABLE"                  # Can be MUTABLE or IMMUTABLE
+  tags = merge(local.default_tags,
+    {
+      "Name" = "${local.name_prefix}-ecr"
+    }
+  )
+}
